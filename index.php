da2a22d30c6b99468f7f5b97e2ee3d18a8df27f7
Waldir Merjildo <muwaldir@gmail.com>
2022-09-09 04:01:43 -0500

    avance d eproyecto minmarket

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<style>
+    body{
+        padding: 0;
+        margin: 0;
+    
+    }
+    .imagencentra{
+        height: 485px;
+    }
+    
+</style>
+
+</head>
+<body>
+<header class="container-fluid d-flex justify-content-around bg-dark text-light py-3">
+    <div>Venta telefonica: +51 9747374833</div>
+    <div>Cart $0 <a href=""><img class="text-light" width="30" height="30" src="img/carro-de-la-carretilla.png" alt=""></a></div>
+   
+</header>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.php">soledad</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="templates/productos.php">Mi cuenta</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="container-fluid bg-success py-2">
+    
+   <!-- Hover added -->
+   <div class="container text-center row mx-auto">
+    <div class="col-md-2">
+        <a href="#" class="col-md-2 list-group-item list-group-item-action ">Bebidas</a>
+    </div>
+    <div class="col-md-2">
+
+        <a href="#" class="col-md-2 list-group-item list-group-item-action">Abarrotes</a>
+    </div>
+    <div class="col-md-2">
+
+    <a href="#" class="col-md-2 list-group-item list-group-item-action">Condimento</a>
+    </div>
+    <div class="col-md-2">
+
+    <a href="#" class="col-md-2 list-group-item list-group-item-action">frutas y Verduras</a>
+    </div>
+    <div class="col-md-2">
+
+    <a href="#" class="col-md-2 list-group-item list-group-item-action">lacteos</a>
+    </div>
+    <div class="col-md-2">
+
+    <a href="#" class="col-md-2 list-group-item list-group-item-action">Mascotas</a>
+    </div>
+   
+   </div>
+    
+</div>
+
+<div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://blog.aepla.es/wp-content/uploads/2020/11/beneficios-frutas-verduras-aepla.jpeg" class="d-block w-100 imagencentra " alt="...">
+    </div>
+    
+  </div>
+</div>
+
+  <footer>
+    
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    </body>
+    </html>
---
+++ templates/bebidas.php
@@ -0,0 +1,118 @@
+<?php
+    include "cabecera.php"
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+
+            <div class="card">
+                <div class="card-header">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">COCA COLA</h4>
+                    <p class="card-text">Tamaño pequeño</p>
+                </div>
+                <div class="card-footer text-muted">
+                    Bebida
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+
+<div class="card">
+    <div class="card-header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">COCA COLA</h4>
+        <p class="card-text">Tamaño pequeño</p>
+    </div>
+    <div class="card-footer text-muted">
+        Bebida
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">COCA COLA</h4>
+        <p class="card-text">Tamaño pequeño</p>
+    </div>
+    <div class="card-footer text-muted">
+        Bebida
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">COCA COLA</h4>
+        <p class="card-text">Tamaño pequeño</p>
+    </div>
+    <div class="card-footer text-muted">
+        Bebida
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">COCA COLA</h4>
+        <p class="card-text">Tamaño pequeño</p>
+    </div>
+    <div class="card-footer text-muted">
+        Bebida
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">COCA COLA</h4>
+        <p class="card-text">Tamaño pequeño</p>
+    </div>
+    <div class="card-footer text-muted">
+        Bebida
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3g04ew-FWlauTamyvv2MHS2kH0GkuDlGREh2fIj1HIFEXwWp8VD4hIl4wXtN-wPhJISw&usqp=CAU" alt="">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">COCA COLA</h4>
+        <p class="card-text">Tamaño pequeño</p>
+    </div>
+    <div class="card-footer text-muted">
+        Bebida
+    </div>
+</div>
+</div>
+        
+    </div>
+</div>
+
+<?php
+include "pie.php"
+?>
